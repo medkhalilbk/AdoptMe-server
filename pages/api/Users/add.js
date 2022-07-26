d4d0b49ba53d860faa190d9634806bcc065cfafa
medkhalilbk <medkhalilbenkhelil@gmail.com>
2022-07-26 21:05:55 +0100

    Make Add no accessible

--- pages/api/Users/add.js
+++ pages/api/Users/add.js
@@ -6,17 +6,21 @@ import User from '../../../utils/userModel';
  * @param {import('next').NextApiResponse} res
  */
 export default async function addTest(req, res) {
-    try {
-        console.log('CONNECTING TO MONGO');
-        await connectMongo();
-        console.log('CONNECTED TO MONGO');
-        console.log('CREATING DOCUMENT');
-        const user = await User.create(req.body);
-        console.log('CREATED DOCUMENT');
+    if (req.method === 'POST') {
+        try {
+            console.log('CONNECTING TO MONGO');
+            await connectMongo();
+            console.log('CONNECTED TO MONGO');
+            console.log('CREATING DOCUMENT');
+            const user = await User.create(req.body);
+            console.log('CREATED DOCUMENT');
 
-        res.json({ user });
-    } catch (error) {
-        console.log(error);
-        res.json({ error });
+            res.json({ user });
+        } catch (error) {
+            console.log(error);
+            res.json({ error });
+        }
+    } else {
+        res.status(403)
     }
 }
