22127f2c0f4a0df614e831f6cc3b22245b10cb2b
medkhalilbk <medkhalilbenkhelil@gmail.com>
2022-12-07 22:06:35 +0100

    show filter (type,age,sex,race,color,region)

--- pages/api/Ad/ShowFilter.js
+++ pages/api/Ad/ShowFilter.js
@@ -10,10 +10,30 @@ export default async function ShowAds(req, res) {
 
         console.log('CONNECTING TO MONGO');
         mongoose.connect('mongodb+srv://khalil:nUhi3qyilDygDhz9@adoptme.igxn7.mongodb.net/?retryWrites=true&w=majority').then(() => {
+            let objectRequest  =  {}
+            let bodyReq = req.body
 
-            let typeByReq = req.body.type
+            if(bodyReq.type !== undefined){
+                objectRequest.type = bodyReq.type
+            }
+            if(bodyReq.age !== undefined){
+                objectRequest.age = bodyReq.age
+            }
+            if(bodyReq.sex !== undefined){
+                objectRequest.sex = bodyReq.sex
+            }
+            if(bodyReq.race !== undefined){
+                objectRequest.race = bodyReq.race
+            }
+            if(bodyReq.color !== undefined){
+                objectRequest.color = bodyReq.color
+            }  if(bodyReq.region !== undefined){
+                objectRequest.region = bodyReq.region
+            }
+
+
             const db = mongoose.connection.db ;
-            db.collection('Ad').find({type:typeByReq}).toArray((err, Ad) => {
+            db.collection('Ad').find(objectRequest).toArray((err, Ad) => {
 
                 res.json({ Ad })
 
